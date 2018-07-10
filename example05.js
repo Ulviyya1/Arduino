3349ab68d8e44b33957072632ec028f27f71f981
Ulviyya <ulviyye.rva@gmail.com>
2018-07-10 13:23:53 +0200

    adding another ip

--- example05.js
+++ example05.js
@@ -3,16 +3,19 @@ var io = require("socket.io").listen(http);
 var fs = require("fs");
 var firmata = require("firmata");
 
-var board = new firmata.Board("dev/ttyACM0", function() { 
+var board = new firmata.Board("/dev/ttyACM0", function() { 
     
     console.log("Connecting to Arduino");
+    
     console.log("Activation of Pin 13");
     board.pinMode(13,board.MODES.OUTPUT);
-})
+    console.log("Activation of Pin 12");
+    board.pinMode(12,board.MODES.OUTPUT);
+});
 
 function handler(req, res)
 {
-    fs.readFile(__dirname + "/example04.html", 
+    fs.readFile(__dirname + "/example05.html", 
     function (err, data) {
         if (err)
         {
@@ -27,14 +30,29 @@ function handler(req, res)
 }
 
 http.listen(8080);
-io.socket.on("connection", function(socket) {
+io.sockets.on("connection", function(socket) {
     socket.on("commandToArduino", function(commandNo){
         if (commandNo == "1") {
         board.digitalWrite(13, board.HIGH);
     }
     if (commandNo == "0") {
-        board.digitalWrite(13, board.LOW);
-        }
+    board.digitalWrite(13, board.LOW);
+      
+    }
+    {
+        
+     if (commandNo == "3") {
+        board.digitalWrite(12, board.HIGH);
+    }
+    if (commandNo == "2") {
+    board.digitalWrite(12, board.LOW);
+    }
+       
+    
+    }    
+    
+    
+
 });
 });
 
